1565bc084d26a271c7baffb2da4e4e2c92ac59d0
Marisa Lim <marisa_lim@pacbell.net>
2015-06-09 18:10:20 -0400

    added a different way to make site x species matrix

--- Make_sitexsp_matrix.r
+++ Make_sitexsp_matrix.r
@@ -5,25 +5,27 @@ setwd(MLwd)
 
 library(raster)
 library(maptools)
+library(reshape2)
 
 myvars <- "bio1.bil"
 myvars <- stack(myvars)
 plot(myvars)
 myvars
 
-myvars2 <- aggregate(myvars, fact=6) #this makes the res=c(1,1)
+myvars2 <- aggregate(myvars, fact=6) #fact=6 makes the res=c(1,1); fact=3 makes the res=c(0.5,0.5)
 myvars2
 plot(myvars2)
 
 # crop extent to just N. America and S. America
 myex<-extent(c(-165, -30, -60, 75))
-myvarscrop <- crop(myvars2, myex)
-class(myvarscrop) # this is a rasterbrick
+myvarscrop <- crop(myvars, myex)
+class(myvarscrop) # this is a rasterlayer
 res(myvarscrop)
 myvarscrop
 plot(myvarscrop)
 
 # make presence/absence 1/0 matrix
+# this includes all the space where hummingbirds don't occur - e.g., ocean, so a lot of absences that I don't need
 siteXsp <- function(){
   #lists all NatureServe files with .shp extension name (n=338 files)
   NSfolder = "Trochilidae"
@@ -67,6 +69,74 @@ length(mysitexspmat[mysitexspmat$Urosticte_ruficrissa == "1", 339])
 length(mysitexspmat[mysitexspmat$Metallura_tyrianthina == "1", 251])
 length(mysitexspmat[mysitexspmat$Selasphorus_rufus == "1", 312])
 
+head(mysitexspmat[mysitexspmat$Loddigesia_mirabilis == "1",230]) #range too small? has 0 presences...the mean of the space must be 0
+
+#try Ben's code:
+siteXsp_Ben <- function(){
+  #create blank raster
+  r <- raster(myvarscrop)
+  
+  #Turn each shapefile into a raster map
+  species_rasterize <- list()
+  
+  #lists all NatureServe files with .shp extension name (n=338 files)
+  NSfolder = "Trochilidae"
+  shpfilenames <- list.files(paste(NSfolder,sep="/"),pattern=".shp",full.name=T,recursive=T)
+  d <- data.frame("Species"="species")
+  #loop through all files
+  for(x in 1:length(shpfilenames)){
+    #load file
+    try(sp_shp <- readShapePoly(shpfilenames[[x]], delete_null_obj=TRUE))
+    
+    #turn to raster
+    sp_ras <- rasterize(sp_shp,r)
+    
+    #which cells are 1's?
+    presence_cells <- Which(sp_ras, cell=TRUE)
+    
+    #check extent, return NA if no presences
+    if(length(presence_cells)==0){
+      species_rasterize[[x]] <- (NA); next}
+    
+    #get xy of presence cells
+    species_rasterize[[x]] <- presence_cells
+    
+    #keep species names for naming columns
+    keepname <- data.frame(Species=sp_shp$SCINAME[1])
+    d <- rbind(d, keepname)
+    
+    print(x)
+  }
+  
+  d <- d[-1,]
+  drename <- strsplit(x=as.character(d), split = " ")
+  drename2 <- sapply(drename, function(x){
+    paste(x[[1]], x[[2]], sep="_")
+  })
+  names(species_rasterize) <- drename2
+  
+  #remove species with just NA (length==1)
+  species_full <- species_rasterize[!sapply(species_rasterize,length)==1]
+  
+  #yields how many species?
+  length(species_full)
+  
+  #melt list into dataframe
+  m.species <- melt(species_full)
+  
+  sitexsppmat <- t(as.data.frame.array(table(m.species$L1, m.species$value)))
+  write.csv(sitexsppmat, "sitexspmat.csv")
+}
+siteXsp_Ben()
+
+mysitexspmat <- read.csv("sitexspmat.csv") 
+head(mysitexspmat)
+dim(mysitexspmat)
+rownames(mysitexspmat) <- mysitexspmat$X
+colnames(mysitexspmat)[1] <- "cellIDs"
+length(mysitexspmat[mysitexspmat$Calypte_anna == "1", 65])
+head(mysitexspmat[mysitexspmat$Urosticte_ruficrissa == "1", 324])
+head(mysitexspmat[mysitexspmat$Loddigesia_mirabilis == "1",217])
 
 # Get cell coords
 cell_coords <- function(){
@@ -78,7 +148,16 @@ cell_coords <- function(){
   # this gets the cellIDs and XY coordinates
   mycoords <- data.frame("cellIDs"=cellIDs, "X"=cellcoords[,1], "Y"=cellcoords[,2])
   
+  # make it match the cellIDs in the sitexsp matrix
+  mycells <- data.frame(cellIDs=mysitexspmat$cellIDs)
+  dim(mycells)
+  dim(mycoords)
+  mycoords2 <- merge(mycoords, mycells, by.y="cellIDs")
+  dim(mycoords2)
+  # head(mycoords2)
+  
   write.csv(mycoords, "cell_coords.csv")
+  write.csv(mycoords2, "cell_coords2.csv")
 }
 cell_coords()
 
@@ -86,7 +165,7 @@ mycellcoords <- read.csv("cell_coords.csv")
 head(mycellcoords)
 
 # Make shape file of extent
-writeRaster(myvarscrop, "myvarscrop.grd")
+writeRaster(myvarscrop, "myvarscrop.grd", overwrite=TRUE)
 
 
 
