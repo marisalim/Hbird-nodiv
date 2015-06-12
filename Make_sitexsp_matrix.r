0bd0e5fe40888e3683885ce0bbf7f8520880c5b2
Marisa Lim <marisa_lim@pacbell.net>
2015-06-11 23:40:01 -0400

    updated code

--- Make_sitexsp_matrix.r
+++ Make_sitexsp_matrix.r
@@ -1,13 +1,15 @@
 #Code for making distribution inputs for nodiv analysis with hummingbird range maps
 
-MLwd = "D:/MarisaLimfiles/Make_sitexsp_matrix/for sitexsp"
+# MLwd = "D:/MarisaLimfiles/Make_sitexsp_matrix/for sitexsp"
+MLwd = "C:/Users/mcwlim/Desktop/StonyBrook/GrahamLab/Dissertation idea materials/THESIS PROJECTS/Div-Biogeo project/"
 setwd(MLwd)
 
 library(raster)
 library(maptools)
 library(reshape2)
 
-myvars <- "bio1.bil"
+#myvars <- "bio1.bil"
+myvars <- "wc10/bio1.bil"
 myvars <- stack(myvars)
 plot(myvars)
 myvars
@@ -18,21 +20,21 @@ plot(myvars2)
 
 # crop extent to just N. America and S. America
 myex<-extent(c(-165, -30, -60, 75))
-myvarscrop <- crop(myvars, myex)
+myvarscrop <- crop(myvars2, myex)
 class(myvarscrop) # this is a rasterlayer
 res(myvarscrop)
 myvarscrop
 plot(myvarscrop)
 
 # make presence/absence 1/0 matrix
-# this includes all the space where hummingbirds don't occur - e.g., ocean, so a lot of absences that I don't need
 siteXsp <- function(){
   #lists all NatureServe files with .shp extension name (n=338 files)
-  NSfolder = "Trochilidae"
+  #NSfolder = "Trochilidae"
+  NSfolder = "Trochilidae2014/Trochilidae"
   shpfilenames <- list.files(paste(NSfolder,sep="/"),pattern=".shp",full.name=T,recursive=T)
   
   # use this statement when testing subset of the data, otherwise comment out
-#   shpfilenames <- shpfilenames[1:5]
+  # shpfilenames <- shpfilenames[1:10]
   
   # this gets the sitexsp matrix of 0s and 1s
   rasters <- list()
@@ -52,17 +54,30 @@ siteXsp <- function(){
     paste(x[[1]], x[[2]], sep="_")
   })
   colnames(x) <- drename2
-  
+
   # #   use these to check that the ranges are properly mapping onto the extent
   #   plot(s) # note: this plots the map of the species on the overall extent
   #   plot(x) # note: this is really slow, it just shows that there are 0s and 1s
-  write.csv(x, "hbird_sitexsp_matrix.csv")
+  
+  # write.csv(x, "hbird_sitexsp_matrix.csv")
+  
+  # version without cells that are presence only (e.g., ocean)
+  m <- as.matrix(x, rownames.force=T) #use rownames.force=T to keep the cellIDs
+  m1 <- m[apply(m, MARGIN=1, FUN=sum)>0, ] #MARGIN=1 means rows
+  
+  # write.csv(m1, "hbird_sitexspmat_sub.csv")
+  write.csv(m1, "C:/Users/mcwlim/Dropbox/resultsfromSarahcomputer/nodesigfiles/hbird_sitexspmat_sub.csv")
+  
 }
 siteXsp()
 
-mysitexspmat <- read.csv("hbird_sitexsp_matrix.csv") # really big file!! will take some time to load
+# mysitexspmat <- read.csv("hbird_sitexsp_matrix.csv") # really big file!! will take some time to load
+# mysitexspmat <- read.csv("hbird_sitexspmat_sub.csv")
+mysitexspmat <- read.csv("C:/Users/mcwlim/Dropbox/resultsfromSarahcomputer/nodesigfiles/hbird_sitexspmat_sub.csv")
 head(mysitexspmat)
+colnames(mysitexspmat)[1] <- "cellIDs"
 dim(mysitexspmat)
+mysitexspmat[1:5,1:5]
 
 #check that there are presences!
 length(mysitexspmat[mysitexspmat$Urosticte_ruficrissa == "1", 339])
@@ -129,14 +144,14 @@ siteXsp_Ben <- function(){
 }
 siteXsp_Ben()
 
-mysitexspmat <- read.csv("sitexspmat.csv") 
-head(mysitexspmat)
-dim(mysitexspmat)
-rownames(mysitexspmat) <- mysitexspmat$X
-colnames(mysitexspmat)[1] <- "cellIDs"
-length(mysitexspmat[mysitexspmat$Calypte_anna == "1", 65])
-head(mysitexspmat[mysitexspmat$Urosticte_ruficrissa == "1", 324])
-head(mysitexspmat[mysitexspmat$Loddigesia_mirabilis == "1",217])
+mysitexspmat2 <- read.csv("sitexspmat.csv") 
+head(mysitexspmat2)
+dim(mysitexspmat2)
+rownames(mysitexspmat2) <- mysitexspmat2$X
+colnames(mysitexspmat2)[1] <- "cellIDs"
+length(mysitexspmat2[mysitexspmat2$Calypte_anna == "1", 65])
+head(mysitexspmat2[mysitexspmat2$Urosticte_ruficrissa == "1", 324])
+head(mysitexspmat2[mysitexspmat2$Loddigesia_mirabilis == "1",217])
 
 # Get cell coords
 cell_coords <- function(){
@@ -150,22 +165,32 @@ cell_coords <- function(){
   
   # make it match the cellIDs in the sitexsp matrix
   mycells <- data.frame(cellIDs=mysitexspmat$cellIDs)
+  # mycells <- data.frame(cellIDs=mysitexspmat2$cellIDs)
   dim(mycells)
   dim(mycoords)
   mycoords2 <- merge(mycoords, mycells, by.y="cellIDs")
   dim(mycoords2)
   # head(mycoords2)
   
-  write.csv(mycoords, "cell_coords.csv")
-  write.csv(mycoords2, "cell_coords2.csv")
+  # round coords to integer
+  mycoords2_rounded <- data.frame(cellIDs=mycoords2$cellIDs, X=round(mycoords2$X), Y=round(mycoords2$Y))
+  
+  # write.csv(mycoords, "cell_coords.csv")
+  # write.csv(mycoords2, "cell_coords2.csv")
+  # write.csv(mycoords2_rounded, "cell_coords_rounded.csv")
+  write.csv(mycoords2_rounded, "C:/Users/mcwlim/Dropbox/resultsfromSarahcomputer/nodesigfiles/cell_coords_rounded.csv")
 }
 cell_coords()
 
-mycellcoords <- read.csv("cell_coords.csv")
+#mycellcoords <- read.csv("cell_coords.csv")
 head(mycellcoords)
 
+#mycoords_rounded <- read.csv("cell_coords_rounded.csv")
+mycoords_rounded <- read.csv("C:/Users/mcwlim/Dropbox/resultsfromSarahcomputer/nodesigfiles/cell_coords_rounded.csv")
+head(mycoords_rounded)
+
+
 # Make shape file of extent
-writeRaster(myvarscrop, "myvarscrop.grd", overwrite=TRUE)
-
-
+# writeRaster(myvarscrop, "myvarscrop.grd", overwrite=TRUE)
+writeRaster(myvarscrop, "C:/Users/mcwlim/Dropbox/resultsfromSarahcomputer/nodesigfiles/myvarscrop.grd", overwrite=TRUE)
 
--- hbird_nodiv.r
+++ hbird_nodiv.r
@@ -1,4 +1,5 @@
-MLwd = "D:/MarisaLimfiles/Make_sitexsp_matrix/"
+#MLwd = "D:/MarisaLimfiles/Make_sitexsp_matrix/"
+MLwd = "C:/Users/mcwlim/Dropbox/resultsfromSarahcomputer/nodesigfiles"
 setwd(MLwd)
 
 # # Installing the package
@@ -15,19 +16,25 @@ library(ape)
 library(raster)
 
 # Building distrib_data and nodiv_data object
-hb_commat <- read.csv("for sitexsp/hbird_sitexsp_matrix.csv") #a 0/1 site-by-species matrix
+#hb_commat <- read.csv("for sitexsp/hbird_sitexspmat_sub.csv") #a 0/1 site-by-species matrix
+hb_commat <- read.csv("hbird_sitexspmat_sub.csv")
 dim(hb_commat)
+hb_commat[1:5,1:4]
+#rownames(hb_commat) <- hb_commat$X
 hb_commat2 <- hb_commat[,-1]
-hb_coords <- read.csv("for sitexsp/cell_coords.csv")[,-1] # a data.frame with one row for each site, and lat/long coordinates in 2 of the columns
-head(hb_coords)
-colnames(hb_coords) <- c("cellIDs", "X", "Y")
 
-# hb_commat2test <- hb_commat2[250613:258933,]
-# hb_coordstest <- hb_coords[250613:258933,]
-# hb_test <- distrib_data(hb_commat2test,hb_coordstest)
+#hb_commat2 without A. colubris
+hb_commat_noAc <- hb_commat2[,!colnames(hb_commat2)=="Archilochus_colubris"]
+
+#hb_coords <- read.csv("for sitexsp/cell_coords_rounded.csv")[,-1] # a data.frame with one row for each site, and lat/long coordinates in 2 of the columns
+hb_coords <- read.csv("cell_coords_rounded.csv")[,-1]
+head(hb_coords)
+#colnames(hb_coords) <- c("cellIDs", "X", "Y")
 
 # The coords do not need to have the same length or be ordered the same way
-hummers <- distrib_data(hb_commat2, hb_coords) #FIXME: CRASHES with 0.1degree raster! I think the data is too big; it's fine when I change the res to 1degree
+hummers <- distrib_data(hb_commat2, hb_coords) #48 species dropped because 0 occurences 
+
+hummers_noAc <- distrib_data(hb_commat_noAc, hb_coords)
 
 # Describing the data set
 hummers
@@ -37,21 +44,42 @@ names(hummers)
 # create a species richness plot as diagnostic
 plot(hummers)  
 
-myshape <- raster("for sitexsp/myvarscrop.grd")
+#myshape <- raster("for sitexsp/myvarscrop.grd")
+myshape <- raster("myvarscrop.grd")
 plot(myshape)
 hummers <- add_shape(hummers, myshape)
+hummers_noAc <- add_shape(hummers_noAc, myshape)
 plot(hummers)
 
 # We can also add the phylogenetic information
-tree <- read.tree("humtree280.tre") 
+#tree <- read.tree("humtree280.tre") 
+tree <- read.tree("C:/Users/mcwlim/Desktop/StonyBrook/GrahamLab/Dissertation idea materials/THESIS PROJECTS/Hummingbird_Genomics/humtree280.tre")
 hummers <- nodiv_data(tree, hummers) 
 summary(hummers)
 
+hummers_noAc <- nodiv_data(tree, hummers_noAc)
+summary(hummers_noAc)
+
 # Now the default plot also shows the phylogeny
-plot(hummers)
 jpeg("Hbird_richness.jpg", height=6, width=6, units="in", res=600)
 plot_richness(hummers, main="Species richness")
 dev.off()
+jpeg("Hbird_noderichness.jpg", height=6, width=10, units="in", res=600)
+par(mfrow=c(1,2))
+plot_node(hummers, node=304, main="Species richness at node 304")
+plot_node(hummers, node=251, main="Species richness at node 251")
+dev.off()
+plot_node(hummers,node=305)
+plot_node(hummers, node=423)
+
+jpeg("Hbird_noderichness_outofSA.jpg", height=6, width=6, units="in", res=600)
+plot_node(hummers,node=384, main="Species richness at node 384")
+dev.off()
+
+plot_richness(hummers_noAc, main="Species richness")
+plot_node(hummers_noAc, node=303)
+plot_node(hummers_noAc, node=250)
+
 
 # The most basic summary metrics are then available:
 head(species(hummers))
@@ -60,6 +88,10 @@ head(sites(hummers))
 Nsites(hummers)
 
 hist(occupancy(hummers))
+
+plot_species(hummers, "Archilochus_colubris")
+plot_species(hummers, "Calypte_anna")
+plot_species(hummers, "Metallura_phoebe")
 
 plot_species(hummers, "Chalcostigma_herrani")
 plot_species(hummers, 16) # species, sites and nodes can all be identified as numbers or names
@@ -98,7 +130,35 @@ jpeg("Hbird_SOS_Basevsrest.jpg", height=6, width=6, units="in", res=600)
 plotSOS(humm_nodiv, 251, main="node 251: HermitsTopazes vs. rest")
 dev.off()
 
+jpeg("Hbird_SOS_hists.jpg", height=6, width=8, units="in", res=600)
+par(mfrow=c(1,2))
 hist(SOS(humm_nodiv, 304))
+hist(SOS(humm_nodiv, 251))
+dev.off()
+
 GND(humm_nodiv)
+
+
+humm_nodiv_noAc <- Node_analysis(hummers_noAc, repeats = 200, method = "rdtable") #TODO: how many repeats?
+summary(humm_nodiv_noAc)
+plot(humm_nodiv_noAc, label=nodenumbers(humm_nodiv_noAc), col="white")
+
+jpeg("Hbird_GND_geographynoAc.jpg", height=8, width=10, units="in", res=600)
+plot(humm_nodiv_noAc, label=humm_nodiv_noAc$GND, col=heat.colors(10), direction="upwards")
+dev.off()
+#plot SOS values for a given node
+jpeg("Hbird_SOS_AndeancladevsrestnoAc.jpg", height=6, width=6, units="in", res=600)
+plotSOS(humm_nodiv_noAc, 303, main="node 303: Andean clade vs. rest") 
+dev.off()
+jpeg("Hbird_SOS_BasevsrestnoAc.jpg", height=6, width=6, units="in", res=600)
+plotSOS(humm_nodiv_noAc, 250, main="node 250: HermitsTopazes vs. rest")
+dev.off()
+
+jpeg("Hbird_SOS_histsnoAc.jpg", height=6, width=8, units="in", res=600)
+par(mfrow=c(1,2))
+hist(SOS(humm_nodiv_noAc, 303))
+hist(SOS(humm_nodiv_noAc, 250))
+dev.off()
+
 
 
